640dd99184dc24528a016faec3d25fe76731d598
audiluqman-god <audiluqman0@gmail.com>
2026-09-21 16:57:55 +0700

    feat: buat class Lingkaran dan rumus

--- src/bangun_datar/Lingkaran_Audi.java
+++ src/bangun_datar/Lingkaran_Audi.java
@@ -9,59 +9,56 @@ package bangun_datar;
  * @author user
  */
 public class Lingkaran_Audi {
-private double id_Lingkaran;
 private double jari_jari;
 private double diameter;
 private double luas;
 private double keliling;
 
-    public Lingkaran_Audi(double id_Lingkaran, double jari_jari, double diameter, double luas, double keliling) {
-        this.id_Lingkaran = id_Lingkaran;
-        this.jari_jari = jari_jari;
-        this.diameter = diameter;
-        this.luas = luas;
-        this.keliling = keliling;
+        public Lingkaran_Audi(double jari_jari, double diameter, double luas, double keliling) {
+            this.jari_jari = jari_jari;
+            this.diameter = diameter;
+            this.luas = luas;
+            this.keliling = keliling;
+        }
+
+        public double getJari_jari() {
+            return jari_jari;
+        }
+
+        public void setJari_jari(double jari_jari) {
+            this.jari_jari = jari_jari;
+        }
+
+        public double getDiameter() {
+            return diameter;
+        }
+
+        public void setDiameter(double diameter) {
+            this.diameter = diameter;
+        }
+
+        public double getLuas() {
+            return luas;
+        }
+
+        public void setLuas(double luas) {
+            this.luas = luas;
+        }
+
+        public double getKeliling() {
+            return keliling;
+        }
+
+        public void setKeliling(double keliling) {
+            this.keliling = keliling;
+        }
+
+        public void tampilHasil(){
+            System.out.println();
+    System.out.println("jari_jari : " + jari_jari);
+    System.out.println("Diameter  : " + diameter);
+    System.out.println("Luas      : " + luas);
+    System.out.println("Keliling  : " + keliling);
+}
         
-    }
-
-    public double getId_Lingkaran() {
-        return id_Lingkaran;
-    }
-
-    public void setId_Lingkaran(double id_Lingkaran) {
-        this.id_Lingkaran = id_Lingkaran;
-    }
-
-    public double getJari_jari() {
-        return jari_jari;
-    }
-
-    public void setJari_jari(double jari_jari) {
-        this.jari_jari = jari_jari;
-    }
-
-    public double getDiameter() {
-        return diameter;
-    }
-
-    public void setDiameter(double diameter) {
-        this.diameter = diameter;
-    }
-
-    public double getLuas() {
-        return luas;
-    }
-
-    public void setLuas(double luas) {
-        this.luas = luas;
-    }
-
-    public double getKeliling() {
-        return keliling;
-    }
-
-    public void setKeliling(double keliling) {
-        this.keliling = keliling;
-    }
-
 }
